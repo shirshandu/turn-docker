f0ea8aa6d5e2497beac4419d61fe86e96c954adc
Shirshandu Bhattacharjee <shirshandu@yahoo.com>
2020-06-24 12:25:40 +0530

    Create start_coturn.sh

---
+++ start_coturn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting TURN/STUN server"
+turnserver -a -v -L 0.0.0.0 -u shirshandu:qwerty --server-name "${TURN_SERVER_NAME}" --realm=${TURN_REALM}  -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
